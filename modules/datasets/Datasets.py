eb9e3d02a724c3416a47f62b19e71b7d4a588269
DannyCollinson <dannycollinson12@gmail.com>
2023-11-22 04:24:25 -0800

    CNN Torch dataset working

--- modules/datasets/Datasets.py
+++ modules/datasets/Datasets.py
@@ -107,7 +107,7 @@ class EmitEcostressDataset(Dataset):
                 )
             else:
                 raise ValueError(
-                    'ecostress_data must be 2- or 3-dimensional, '
+                    'ecostress_data must be 1- or 2-dimensional, '
                     f'found {len(ecostress_data.shape)}-dimensional'
                 )
         else:
@@ -126,13 +126,12 @@ class EmitEcostressDataset(Dataset):
             self.ecostress_center = np.mean(self.ecostress_data, axis=0)
             self.ecostress_scale = np.std(a=self.ecostress_data, axis=0)
         elif ecostress_center is not None:
-            raise ValueError(
-                'Both center and scale must be provided, but scale was None'
-            )
+            self.ecostress_center = ecostress_center
+            self.ecostress_scale = np.std(a=self.ecostress_data, axis=0)
         else:
-            raise ValueError(
-                'Both center and scale must be provided, but center was None'
-            )
+            self.ecostress_center = np.mean(self.ecostress_data, axis=0)
+            self.ecostress_scale = ecostress_scale
+        
         self.ecostress_data = (
             (
                 self.ecostress_data - self.ecostress_center
@@ -157,13 +156,6 @@ class EmitEcostressDataset(Dataset):
                 'No nan values are allowed: ecostress_data has'
                 f'{np.sum(np.isnan(self.ecostress_data))} nan values.'
             )
-        # nan_mask = np.isnan(self.emit_data)
-        # self.emit_data = self.emit_data[~nan_mask]
-        # self.ecostress_data = self.ecostress_data[~nan_mask[:,0]]
-
-        # assert self.emit_data.shape[0] == self.ecostress_data.shape[0], \
-        #     'emit_data and ecostress_data changed lengths during filtering, ' \
-        #     f'got {self.emit_data.shape[0]} and {self.ecostress_data.shape[0]}'
 
         self.additional_data = []
         if additional_data is not None:
@@ -224,14 +216,13 @@ class EmitEcostressDataset(Dataset):
                 else:
                     raise ValueError(
                         f'Item loaded from index {i} in additional_data_paths '
-                        ' must be 1-. 2-, or 3-dimensional, found '
+                        ' must be 1-, 2-, or 3-dimensional, found '
                         f'{len(additional_data_element.shape)}-dimensional'
                     )
 
         for i, additional_data_element in (
             enumerate(iterable=self.additional_data)
         ):
-            # additional_data_element = additional_data_element[~nan_mask]
             if i >= len(self.additional_data):
                 index: int = i - len(self.additional_data)
                 message: str = f'loaded from index {index} in additional_data_paths'
@@ -283,21 +274,6 @@ class EmitEcostressDataset(Dataset):
         return x, y
 
 
-
-'''
-Dataset classes for use in model training
-'''
-
-from typing import Union
-import pickle
-
-import numpy as np
-
-from torch import Tensor, tensor
-from torch import concatenate, unsqueeze
-from torch.utils.data import Dataset
-
-
 class CNNDataset(Dataset):
     def __init__(
         self,
@@ -310,7 +286,8 @@ class CNNDataset(Dataset):
         ecostress_scale: Union[float, None] = None,
         additional_data_paths: Union[tuple[str], None] = None,
         additional_data: Union[tuple[np.ndarray], None] = None,
-        device: str = 'cpu',
+        y_size: int = 50,
+        x_size: int = 50,
     ) -> None:
         '''
         Builds a pytorch dataset for predicting ECOSTRESS LST
@@ -318,12 +295,12 @@ class CNNDataset(Dataset):
 
         Input:
         emit_data_path: path to emit data as .npy or .pkl file,
-                        not used if emit_data is not None
+                        not used if emit_data is not None *
         emit_data: 3-dimensional np array of emit data
         omit_components: int telling how many components of the emit
                          spectra being loaded/provided to omit rom the end
         ecostress_data_path: path to ecostress data as .npy or .pkl file,
-                             not used if ecostress_data is not None
+                             not used if ecostress_data is not None *
         ecostress_data: 2-dimensional np array of ecostress data
         ecostress_center: float to center ecostress_data with
         ecostress_scale: float to scale centered ecostress_data by
@@ -332,6 +309,8 @@ class CNNDataset(Dataset):
                                must be .npy or .pkl files
         additional_data: tuple of 2-, or 3-dimensional np arrays of
                          supplementary data used for additional model input
+        y_size: number of pixels each training image is in the y-direction
+        x_size: number of pixels each training image is in the x-direction
 
         * Note that emit_data and ecostress_data will take precedence
           over emit_data_path and ecostress_data_path, respectively,
@@ -351,22 +330,15 @@ class CNNDataset(Dataset):
                 )
         self.omit_components = omit_components
         if emit_data is not None:
-            if len(emit_data.shape) == 2:
+            if len(emit_data.shape) == 3:
                 self.emit_data = emit_data
                 if self.omit_components > 0:
-                    self.emit_data = self.emit_data[:, :-self.omit_components]
-            elif len(emit_data.shape) == 3:
-                self.emit_data = emit_data.reshape(
-                    (
-                        emit_data.shape[0] * emit_data.shape[1],
-                        emit_data.shape[2]
+                    self.emit_data = (
+                        self.emit_data[:, :, :-self.omit_components]
                     )
-                )
-                if self.omit_components > 0:
-                    self.emit_data = self.emit_data[:, :-self.omit_components]
             else:
                 raise ValueError(
-                    'emit_data must be 2- or 3-dimensional, '
+                    'emit_data must be 3-dimensional, '
                     f'found {len(emit_data.shape)}-dimensional'
                 )
         else:
@@ -385,15 +357,11 @@ class CNNDataset(Dataset):
                     'invalid file extension, must be .npy or .pkl'
                 )
         if ecostress_data is not None:
-            if len(ecostress_data.shape) == 1:
+            if len(ecostress_data.shape) == 2:
                 self.ecostress_data = ecostress_data
-            elif len(ecostress_data.shape) == 2:
-                self.ecostress_data = ecostress_data.reshape(
-                    ecostress_data.shape[0] * ecostress_data.shape[1]
-                )
             else:
                 raise ValueError(
-                    'ecostress_data must be 2- or 3-dimensional, '
+                    'ecostress_data must be 2-dimensional, '
                     f'found {len(ecostress_data.shape)}-dimensional'
                 )
         else:
@@ -402,8 +370,13 @@ class CNNDataset(Dataset):
             )
 
         assert self.emit_data.shape[0] == self.ecostress_data.shape[0], \
-            'emit_data and ecostress_data must have the length, ' \
-            f'got {self.emit_data.shape[0]} and {self.ecostress_data.shape[0]}'
+            'emit_data and ecostress_data must have the same ' \
+            f'first dimension, got shapes {self.emit_data.shape}' \
+            f'and {self.ecostress_data.shape}'
+        assert self.emit_data.shape[1] == self.ecostress_data.shape[1], \
+            'emit_data and ecostress_data must have the same ' \
+            f'second dimension, got shapes {self.emit_data.shape}' \
+            f'and {self.ecostress_data.shape}'
 
         if ecostress_center is not None and ecostress_scale is not None:
             self.ecostress_center = ecostress_center
@@ -412,13 +385,12 @@ class CNNDataset(Dataset):
             self.ecostress_center = np.mean(self.ecostress_data, axis=0)
             self.ecostress_scale = np.std(a=self.ecostress_data, axis=0)
         elif ecostress_center is not None:
-            raise ValueError(
-                'Both center and scale must be provided, but scale was None'
-            )
+            self.ecostress_center = ecostress_center
+            self.ecostress_scale = np.std(a=self.ecostress_data, axis=0)
         else:
-            raise ValueError(
-                'Both center and scale must be provided, but center was None'
-            )
+            self.ecostress_center = np.mean(self.ecostress_data, axis=0)
+            self.ecostress_scale = ecostress_scale
+
         self.ecostress_data = (
             (
                 self.ecostress_data - self.ecostress_center
@@ -443,35 +415,24 @@ class CNNDataset(Dataset):
                 'No nan values are allowed: ecostress_data has'
                 f'{np.sum(np.isnan(self.ecostress_data))} nan values.'
             )
-        # nan_mask = np.isnan(self.emit_data)
-        # self.emit_data = self.emit_data[~nan_mask]
-        # self.ecostress_data = self.ecostress_data[~nan_mask[:,0]]
-
-        # assert self.emit_data.shape[0] == self.ecostress_data.shape[0], \
-        #     'emit_data and ecostress_data changed lengths during filtering, ' \
-        #     f'got {self.emit_data.shape[0]} and {self.ecostress_data.shape[0]}'
 
         self.additional_data = []
         if additional_data is not None:
             for i in range(len(additional_data)):
-                if len(additional_data[i].shape) == 1:
-                    self.additional_data.append(additional_data[i])
-                elif len(additional_data[i].shape) == 2:
-                    self.additional_data.append(additional_data[i])
-                elif len(additional_data[i].shape) == 3:
+                if len(additional_data[i].shape) == 2:
                     self.additional_data.append(
                         additional_data[i].reshape(
-                            (
-                                additional_data[i].shape[0] *
-                                additional_data[i].shape[1],
-                                additional_data[i].shape[2]
-                            )
+                            additional_data[i].shape[0],
+                            additional_data[i].shape[1],
+                            1,
                         )
                     )
+                elif len(additional_data[i].shape) == 3:
+                    self.additional_data.append(additional_data[i])
                 else:
                     raise ValueError(
                         f'Item at index {i} in additional_data must be '
-                        '1-, 2-, or 3-dimensional, '
+                        '2-, or 3-dimensional, '
                         f'found {len(additional_data[i].shape)}-dimensional'
                     )
 
@@ -489,35 +450,26 @@ class CNNDataset(Dataset):
                         'of additional_data_paths has an invalid '
                         'file extension, must be .npy or .pkl'
                     )
-                if len(additional_data_element.shape) == 1:
-                    self.additional_data.append(
-                        additional_data_element.reshape(
-                            (additional_data_element.shape[0], 1)
-                        )
-                    )
                 if len(additional_data_element.shape) == 2:
-                    self.additional_data.append(additional_data_element)
-                elif len(additional_data_element.shape) == 3:
                     self.additional_data.append(
                         additional_data_element.reshape(
-                            (
-                                additional_data_element.shape[0] *
-                                additional_data_element.shape[1],
-                                additional_data_element.shape[2]
-                            )
+                            additional_data_element.shape[0],
+                            additional_data_element.shape[1],
+                            1,
                         )
                     )
+                elif len(additional_data_element.shape) == 3:
+                    self.additional_data.append(additional_data_element)
                 else:
                     raise ValueError(
                         f'Item loaded from index {i} in additional_data_paths '
-                        ' must be 1-. 2-, or 3-dimensional, found '
+                        ' must be 2-, or 3-dimensional, found '
                         f'{len(additional_data_element.shape)}-dimensional'
                     )
 
         for i, additional_data_element in (
             enumerate(iterable=self.additional_data)
         ):
-            # additional_data_element = additional_data_element[~nan_mask]
             if i >= len(self.additional_data):
                 index: int = i - len(self.additional_data)
                 message: str = f'loaded from index {index} in additional_data_paths'
@@ -526,44 +478,87 @@ class CNNDataset(Dataset):
                 message = f'from index {index} in additional_data'
             assert additional_data_element.shape[0] == self.emit_data.shape[0],\
                 f'emit_data and additonal data {message} must have the same ' \
-                f'length, got {self.emit_data.shape[0]} ' \
-                f'and {additional_data_element.shape[0]}'
+                f'first dimension, got shapes {self.emit_data.shape} ' \
+                f'and {additional_data_element.shape}'
+            assert additional_data_element.shape[1] == self.emit_data.shape[1],\
+                f'emit_data and additonal data {message} must have the same ' \
+                f'second dimension, got shapes {self.emit_data.shape} ' \
+                f'and {additional_data_element.shape}'
 
         self.input_dim: int = (
-            self.emit_data.shape[1] +
+            self.emit_data.shape[2] +
             sum(
                 [
-                    additional_data_element.shape[1] if (
-                        len(additional_data_element.shape) > 1
+                    additional_data_element.shape[2] if (
+                        len(additional_data_element.shape) == 3
                     ) else 1
                     for additional_data_element in self.additional_data
                 ]
             )
         )
-
-        self.device = device
-        self.emit_data = tensor(self.emit_data, device=self.device)
-        self.ecostress_data = tensor(self.ecostress_data, device=self.device)
+        
+        if emit_data.shape[0] % y_size != 0:
+            if emit_data.shape[1] % y_size != 0:
+                raise ValueError(
+                    'y_size must divide length of emit first dimension, ' \
+                    f'got y_size {y_size} and length {emit_data.shape[0]}, ' \
+                    'and x_size must divide length of emit second dimension, ' \
+                    f'got x_size {x_size} and length {emit_data.shape[1]}.'
+                )
+            else:
+                raise ValueError(
+                    'y_size must divide length of emit first dimension, ' \
+                    f'got y_size {y_size} and length {emit_data.shape[0]}.'
+                )
+        else:
+            if emit_data.shape[1] % x_size != 0:
+                raise ValueError(
+                    'x_size must divide length of emit second dimension, ' \
+                    f'got x_size {x_size} and length {emit_data.shape[1]}.'
+                )
+        
+        self.x_size = x_size
+        self.y_size = y_size
+        
+        self.n_images_y = emit_data.shape[0] // self.y_size
+        self.n_images_x = emit_data.shape[1] // self.x_size
+        
+        self.emit_data = tensor(self.emit_data)
+        self.ecostress_data = tensor(self.ecostress_data)
         for i, additional_data_element in enumerate(self.additional_data):
-            self.additional_data[i] = tensor(
-                additional_data_element, device=self.device
-            )
+            self.additional_data[i] = tensor(additional_data_element)
 
 
     def __len__(self) -> int:
-        return self.emit_data.shape[0]
+        return self.n_images_y * self.n_images_x
 
 
     def __getitem__(self, index: int) -> tuple[Tensor, Tensor]:
-        x = self.emit_data[index, :]
+        row = index // self.n_images_x
+        column = index % self.n_images_x
+        
+        x = self.emit_data[
+            row * self.y_size : (row + 1) * self.y_size,
+            column * self.x_size : (column + 1) * self.x_size,
+            :,
+        ]
+        
         for additional_data_element in self.additional_data:
-            if len(additional_data_element.shape) > 1:
-                x = concatenate(
-                    [x, additional_data_element[index, :]], dim=1
-                )
-            elif len(additional_data_element.shape) == 1:
-                x = concatenate(
-                    [x, unsqueeze(additional_data_element[index], 0)], dim=-1
-                )
-        y = self.ecostress_data[index]
+            x = concatenate(
+                [
+                    x,
+                    additional_data_element[
+                        row * self.y_size : (row + 1) * self.y_size,
+                        column * self.x_size : (column + 1) * self.x_size,
+                        :,
+                    ]
+                ],
+                dim=2,
+            )
+
+        y = self.ecostress_data[
+            row * self.y_size : (row + 1) * self.y_size,
+            column * self.x_size : (column + 1) * self.x_size,
+        ]
+                
         return x, y
