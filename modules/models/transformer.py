924dfd5f1981edc8f3425e978fe2023bdb1028ff
Gabriella Twombly <gtwombly@caltech.edu>
2023-12-07 22:04:38 -0800

    random forest and transformer (WIP)

--- modules/models/transformer.py
+++ modules/models/transformer.py
@@ -1,43 +1,94 @@
+import torch
 import torch.nn as nn
-import torch.nn.functional as F
-from torch import Tensor
-import torch.optim as optim
+import lightning.pytorch as pl
+from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+from datetime import datetime
+import os
+from torch.optim.lr_scheduler import ReduceLROnPlateau
 
-class TransformerModel(nn.Module):
+class TransformerWrapper:
+    def __init__(self, input_dim, output_dim):
+        self.model = Transformer(input_dim, output_dim)
+
+    def create_dataloader(self, dataset):
+        dataloader = DataLoader(
+            dataset,
+            batch_size=2048,
+            shuffle=True,
+            drop_last=True,
+        )
+        return dataloader
+
+    def create_trainer(self) -> pl.Trainer:
+        trainer = pl.Trainer(
+            max_epochs=25,
+            num_sanity_val_steps=0,
+            # logger=None,
+            # check_val_every_n_epoch=1,
+        )
+        return trainer
+
+    def fit(self, train_data, val_data = None):
+        if val_data is None:
+            train_data = self.create_dataloader(train_data)
+            trainer = self.create_trainer()
+            trainer.fit(self.model, train_data)
+        else:
+            train_dataloader = self.create_dataloader(train_data)
+            val_dataloader = self.create_dataloader(val_data)
+            trainer = self.create_trainer()
+            trainer.fit(self.model, train_dataloader, val_dataloader)
+
+
+    def predict(self):
+        pass
+
+    def save(self):
+        pass
+
+class Transformer(pl.LightningModule):
     def __init__(self, input_dim, output_dim, hidden_dim=128, num_layers=4, num_heads=4):
-        super(TransformerModel, self).__init__()
-        
-        # Embedding layer
+        super(Transformer, self).__init__()
         self.embedding = nn.Linear(input_dim, hidden_dim)
-        
-        # Transformer Encoder
         encoder_layers = nn.TransformerEncoderLayer(hidden_dim, num_heads)
         self.transformer_encoder = nn.TransformerEncoder(encoder_layers, num_layers)
-        
-        # Fully connected layer for output
         self.fc = nn.Linear(hidden_dim, output_dim)
+
+        self.train_losses = []
+        self.val_losses = []
     
     def forward(self, x):
-        # Embedding layer
         x = self.embedding(x)
-        
-        # Transformer Encoder
         x = self.transformer_encoder(x)
-        
-        # Global average pooling
         x = x.mean(dim=1)
-        
-        # Fully connected layer for output
         x = self.fc(x)
-        
         return x
 
-# Define the input and output dimensions
-input_dim = 244
-output_dim = 1
+    def training_step(self, batch, batch_idx):
+        inputs, targets = batch
+        outputs = self(inputs)
+        loss = nn.MSELoss()(outputs, targets)
+        self.train_losses.append(loss)
+        return loss
 
-# Create an instance of the Transformer model
-model = TransformerModel(input_dim, output_dim)
+    def validation_step(self, batch, batch_idx):
+        inputs, targets = batch
+        outputs = self(inputs)
+        loss = nn.MSELoss()(outputs, targets)
+        self.val_losses.append(loss)
+        return loss
 
-# Print the model architecture
-print(model)
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), lr=0.001)
+        # optimizer = torch.optim.Adam(self.parameters(), lr=0.001)
+        # scheduler = ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=5)
+        # return {
+        #     'optimizer': optimizer,
+        #     'lr_scheduler': {
+        #         'scheduler': scheduler,
+        #         'monitor': 'val_loss'
+        #     }
+        # }
